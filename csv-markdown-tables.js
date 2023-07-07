5e63b310634243273e90faf8de29b3fd1cc20a18
Robert Brestle <68937744+robertbrestle@users.noreply.github.com>
2023-07-06 20:21:35 -0500

    added support for custom delimiter

--- csv-markdown-tables.js
+++ csv-markdown-tables.js
@@ -6,8 +6,8 @@ MarkdownTablesJS = {
      * @param {string} firstRowHeaderId - input checkbox identifier
      * @param {string} outputId - output textarea identifier
      */
-    initForm: function(formId, inputId, firstRowHeaderId, outputId) {
-        if (typeof formId === "undefined" || typeof inputId === "undefined" || typeof firstRowHeaderId === "undefined" || typeof outputId === "undefined") {
+    initForm: function(formId, inputId, outputId, firstRowHeaderId, delimiterId) {
+        if (typeof formId === "undefined" || typeof inputId === "undefined" || typeof outputId === "undefined" || typeof firstRowHeaderId === "undefined" || typeof delimiterId === "undefined") {
             console.error("Invalid init parameters");
             return null;
         }
@@ -17,7 +17,8 @@ MarkdownTablesJS = {
 
             let inputText = document.getElementById(inputId).value;
             let firstRowHeaderBool = document.getElementById(firstRowHeaderId).checked;
-            let convert = MarkdownTablesJS.convert(inputText, firstRowHeaderBool);
+            let delimiter = document.getElementById(delimiterId).value;
+            let convert = MarkdownTablesJS.convert(inputText, firstRowHeaderBool, delimiter);
             document.getElementById(outputId).value = convert;
         });
     }
@@ -25,18 +26,25 @@ MarkdownTablesJS = {
     /**
      * Convert a CSV string to a markdown table string.
      * @param {string} csvText - CSV table
-     * @param {boolean} firstRowHeaders - if the first row should be treated as the column headers
+     * @param {boolean} firstRowHeader - if the first row should be treated as the column headers
+     * @param {delimiter} delimiter - 
      * @returns {string} markdown table
      */
-    convert: function(csvText, firstRowHeaders) {
+    convert: function(csvText, firstRowHeader, delimiter) {
         let response = "";
 
         let rows = csvText.split("\n");
         let firstRow = 1; // default data to the second row
+        // set delimiter
+        if (typeof delimiter === "undefined" || delimiter == null || delimiter == "") {
+            delimiter = ",";
+        }
+        let delimiterRegexp = new RegExp(delimiter, "g");
+        let notDelimiterRegexp = new RegExp("[^" + delimiter + "]*", "g");
 
-        if (firstRowHeaders) {
+        if (firstRowHeader) {
             // header row
-            response += rows[0].trim().replace(/,/g, " | ") + "\n";
+            response += rows[0].trim().replace(delimiterRegexp, " | ") + "\n";
         }else {
             let headerRow = [];
             let firstRowLength = rows[0].split(",").length;
@@ -49,10 +57,10 @@ MarkdownTablesJS = {
         }
 
         // spacer row
-        response += rows[0].replace(/[^,]*/g, "---").replace(/,/g, " | ") + "\n";
+        response += rows[0].replace(notDelimiterRegexp, "---").replace(delimiterRegexp, " | ") + "\n";
 
         for(let i = firstRow; i < rows.length; i++) {
-            response += rows[i].trim().replace(/,/g, " | ") + "\n";
+            response += rows[i].trim().replace(delimiterRegexp, " | ") + "\n";
         }
 
         return response;
